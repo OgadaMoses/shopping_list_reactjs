31a02a3e2181907777e661ef50e71614929f9491
Ogada Moses <ogadamoses59@gmail.com>
2023-03-19 17:43:02 +0300

    Finish developing shopping list application

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Container from '@mui/material/Container';
 import AppBar  from '@mui/material/AppBar';
 import ToolBar from '@mui/material/Toolbar';
 import Typography  from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import './App.css';
-
-
-
 
 
 function App() {
@@ -22,7 +23,20 @@ const addItem = (item) => {setItems([item, ...items])};
         </Typography>
       </ToolBar>
     </AppBar>
+    <Stack alignItems="Center">
     <AddItem addItem={addItem}/>
+    <List>
+      {
+        items.map((item, index) =>
+        <ListItem key={index} divider>
+          <ListItemText 
+          primary={item.product}
+          secondary={item.amount} />
+        </ListItem>
+        )
+      }
+    </List>
+    </Stack>
    </Container>
   );
 }
